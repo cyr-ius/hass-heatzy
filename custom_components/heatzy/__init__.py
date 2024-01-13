290542592394f99f276c33f44911622c02e7a7bf
cyr-ius <cyr-ius@ipocus.net>
2024-01-13 14:48:29 +0100

    Add update listener

--- custom_components/heatzy/__init__.py
+++ custom_components/heatzy/__init__.py
@@ -19,6 +19,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = HeatzyDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
+    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
     hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
