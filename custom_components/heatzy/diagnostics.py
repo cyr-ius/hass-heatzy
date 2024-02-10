e9d04cc58cbcff8dcab03b3d81b95db60e0433da
cyr-ius <cyr-ius@ipocus.net>
2024-02-10 13:05:04 +0100

    Add diag for heatzy model

--- custom_components/heatzy/diagnostics.py
+++ custom_components/heatzy/diagnostics.py
@@ -94,17 +94,24 @@ async def test_diag_v1(coordinator, devices, entry, hass):
             )
             await asyncio.sleep(2)
             device = await api.async_get_device(did)
-            diag_v1.append([1, device.get("attrs", {}).get("mode")])
+            diag_v1.append([2, device.get("attrs", {}).get("mode")])
+
+            await ws.async_control_device(
+                did, {"raw": [0, 0, 0, 3, 7, 0, 0, 144, 1, 1, 3]}
+            )
+            await asyncio.sleep(2)
+            device = await api.async_get_device(did)
+            diag_v1.append([3, device.get("attrs", {}).get("mode")])
 
             await ws.async_control_device(did, {"raw": "\u505c\u6b62"})
             await asyncio.sleep(2)
             device = await api.async_get_device(did)
-            diag_v1.append([2, device.get("attrs", {}).get("mode")])
+            diag_v1.append([4, device.get("attrs", {}).get("mode")])
 
             await ws.async_control_device(did, "\u505c\u6b62")
             await asyncio.sleep(2)
             device = await api.async_get_device(did)
-            diag_v1.append([3, device.get("attrs", {}).get("mode")])
+            diag_v1.append([5, device.get("attrs", {}).get("mode")])
 
     await ws.async_disconnect()
     task.cancel()
