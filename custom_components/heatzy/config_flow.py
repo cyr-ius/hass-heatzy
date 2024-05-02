83e1a014d04615e43cb29f33ab360cc28ebd7fdb
cyr-ius <cyr-ius@ipocus.net>
2024-05-02 13:05:23 +0200

    Fix lint

--- custom_components/heatzy/config_flow.py
+++ custom_components/heatzy/config_flow.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 import logging
 
-import voluptuous as vol
 from heatzypy import HeatzyClient
 from heatzypy.exception import AuthenticationFailed, HeatzyException, HttpRequestFailed
+import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
