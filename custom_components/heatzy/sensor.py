d3871d4ed09f645f29c4beacdfc9f9ce8e6a1849
cyr-ius <cyr-ius@ipocus.net>
2026-02-01 14:43:45 +0100

    Fix presence sensor

--- custom_components/heatzy/sensor.py
+++ custom_components/heatzy/sensor.py
@@ -1,8 +1,9 @@
 """Sensor for heatzy."""
 
+from collections.abc import Callable
 from dataclasses import dataclass
+from typing import Any, Final
 
-from homeassistant.components.climate import PRESET_ECO
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
@@ -12,15 +13,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import HeatzyDataUpdateCoordinator
-from .const import (
-    CONF_CUR_MODE,
-    CONF_DEROG_MODE,
-    CONF_PRODUCT_KEY,
-    PILOTE_PRO_V1,
-    PRESET_COMFORT_1,
-    PRESET_COMFORT_2,
-)
+from .const import CONF_CUR_MODE, CONF_DEROG_MODE, CONF_PRODUCT_KEY, PILOTE_PRO_V1
 from .entity import HeatzyEntity
 
 
@@ -29,14 +22,20 @@ class HeatzySensorEntityDescription(SensorEntityDescription):
     """Represents an Flow Sensor."""
 
     attr: str | None = None
+    products: list[str]
+    cls: Callable[..., Any]
 
 
-ABSENCE = HeatzySensorEntityDescription(
-    key="absence",
-    name="Absence duration",
-    translation_key="absence",
-    icon="mdi:location-exit",
-    device_class=SensorDeviceClass.DURATION,
+SENSORS_TYPE: Final[tuple[HeatzySensorEntityDescription, ...]] = (
+    HeatzySensorEntityDescription(
+        key="absence",
+        name="Absence duration",
+        translation_key="absence",
+        products=PILOTE_PRO_V1,
+        icon="mdi:location-exit",
+        device_class=SensorDeviceClass.DURATION,
+        cls=lambda *args: PresenceSensor(*args),
+    ),
 )
 
 
@@ -51,8 +50,10 @@ async def async_setup_entry(
 
     for unique_id, device in coordinator.data.items():
         product_key = device.get(CONF_PRODUCT_KEY)
-        if product_key in PILOTE_PRO_V1:
-            entities.extend([PresenceSensor(coordinator, ABSENCE, unique_id)])
+        for description in SENSORS_TYPE:
+            cls = description.cls
+            if product_key in description.products:
+                entities.extend([cls(coordinator, description, unique_id)])
 
     async_add_entities(entities)
 
@@ -62,31 +63,22 @@ class PresenceSensor(HeatzyEntity, SensorEntity):
 
     entity_description: HeatzySensorEntityDescription
 
-    def __init__(
-        self,
-        coordinator: HeatzyDataUpdateCoordinator,
-        description: HeatzySensorEntityDescription,
-        did: str,
-    ) -> None:
-        """Initialize."""
-        super().__init__(coordinator, description, did)
-
     @property
     def native_value(self) -> int | None:
         """Presence status."""
         if (
             self._attrs.get(CONF_DEROG_MODE) == 3
-            and self._attrs.get(CONF_CUR_MODE) == PRESET_COMFORT_1
+            and self._attrs.get(CONF_CUR_MODE) == "cft1"
         ):
             return 30
         if (
             self._attrs.get(CONF_DEROG_MODE) == 3
-            and self._attrs.get(CONF_CUR_MODE) == PRESET_COMFORT_2
+            and self._attrs.get(CONF_CUR_MODE) == "cft2"
         ):
             return 60
         if (
             self._attrs.get(CONF_DEROG_MODE) == 3
-            and self._attrs.get(CONF_CUR_MODE) == PRESET_ECO
+            and self._attrs.get(CONF_CUR_MODE) == "eco"
         ):
             return 90
         return None
