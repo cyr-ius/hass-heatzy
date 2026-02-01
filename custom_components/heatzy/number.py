a91b457a7f633cdd665ea427b048a96476cfe058
cyr-ius <cyr-ius@ipocus.net>
2026-02-01 14:43:05 +0100

    Fix lint

--- custom_components/heatzy/number.py
+++ custom_components/heatzy/number.py
@@ -1,10 +1,10 @@
 """Number platform."""
 
-from __future__ import annotations
-
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Final
+from typing import Any, Final
 
+from homeassistant.components.climate import PRESET_BOOST
 from homeassistant.components.number import (
     NumberDeviceClass,
     NumberEntityDescription,
@@ -15,7 +15,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import HeatzyConfigEntry, HeatzyDataUpdateCoordinator
-from .const import CONF_ATTRS, CONF_DEROG_MODE
+from .const import ALL, CONF_PRODUCT_KEY, PRESET_VACATION
 from .entity import HeatzyEntity
 
 
@@ -23,7 +23,9 @@ from .entity import HeatzyEntity
 class HeatzyNumberEntityDescription(NumberEntityDescription):
     """Represents an Flow Sensor."""
 
-    attr: str = CONF_DEROG_MODE
+    attr: str
+    products: list[str]
+    cls: Callable[..., Any] = lambda *args: HeatzyNumber(*args)
 
 
 NUMBER_TYPES: Final[tuple[HeatzyNumberEntityDescription, ...]] = (
@@ -32,22 +34,26 @@ NUMBER_TYPES: Final[tuple[HeatzyNumberEntityDescription, ...]] = (
         name="Vacation",
         icon="mdi:island",
         translation_key="vacation",
+        products=ALL,
         device_class=NumberDeviceClass.DURATION,
         native_step=1,
         native_unit_of_measurement=UnitOfTime.DAYS,
         native_min_value=1,
         native_max_value=255,
+        attr = PRESET_VACATION
     ),
     HeatzyNumberEntityDescription(
         key="boost",
         name="Boost",
         icon="mdi:thermometer-high",
+        products=ALL,
         translation_key="boost",
         device_class=NumberDeviceClass.DURATION,
         native_step=1,
         native_unit_of_measurement=UnitOfTime.MINUTES,
         native_min_value=1,
         native_max_value=255,
+        attr= PRESET_BOOST
     ),
 )
 
@@ -61,13 +67,15 @@ async def async_setup_entry(
     coordinator = entry.runtime_data
     entities = []
     for unique_id, device in coordinator.data.items():
+        product_key = device.get(CONF_PRODUCT_KEY)
         for description in NUMBER_TYPES:
-            if device.get(CONF_ATTRS, {}).get(description.attr) is not None:
-                entities.extend([Number(coordinator, description, unique_id)])
+            cls = description.cls
+            if product_key in description.products:
+                entities.extend([cls(coordinator, description, unique_id)])
     async_add_entities(entities)
 
 
-class Number(HeatzyEntity, RestoreNumber):
+class HeatzyNumber(HeatzyEntity, RestoreNumber):
     """Number entity."""
 
     def __init__(
@@ -96,6 +104,6 @@ class Number(HeatzyEntity, RestoreNumber):
 
     async def async_set_native_value(self, value: float) -> None:
         """Set new value."""
-        self._device[self.entity_description.key] = value
+        self._device[self.entity_description.attr] = value
         self._resolve_state = value
         self.async_write_ha_state()
